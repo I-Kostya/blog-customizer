b9a3f2c5ac9e96455aa9a910fe3848e7563886c9
Kostya Ischuk <slubrusher@yandex.ru>
2024-09-03 23:09:26 +0700

    added form opening handler

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,19 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [isFormOpen, setIsFormOpen] = useState(false);
+
+	const toggleForm = () => setIsFormOpen(!isFormOpen);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton onClick={toggleForm} isOpen={isFormOpen} />
+			<aside
+				className={clsx(styles.container, isFormOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
