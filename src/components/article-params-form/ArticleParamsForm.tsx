421a776c7f52c9627c09004d98fb8b577d1b2c2a
Kostya Ischuk <slubrusher@yandex.ru>
2024-09-06 00:20:19 +0700

    added handler outside click

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,16 +2,25 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import clsx from 'clsx';
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
 
 export const ArticleParamsForm = () => {
 	const [isFormOpen, setIsFormOpen] = useState(false);
 
+	const rootRef = useRef<HTMLDivElement>(null);
+
 	const toggleForm = () => setIsFormOpen(!isFormOpen);
 
+	useOutsideClickClose({
+		isOpen: isFormOpen,
+		rootRef,
+		onChange: setIsFormOpen,
+	});
+
 	return (
-		<>
+		<div ref={rootRef}>
 			<ArrowButton onClick={toggleForm} isOpen={isFormOpen} />
 			<aside
 				className={clsx(styles.container, isFormOpen && styles.container_open)}>
@@ -22,6 +31,6 @@ export const ArticleParamsForm = () => {
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
